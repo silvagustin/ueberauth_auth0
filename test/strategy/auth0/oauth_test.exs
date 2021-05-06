41612c7e8e8e74eb4c74240206dfea6b0d479d0e
“Agustin <silva.agustinn@gmail.com>
2021-05-06 12:44:17 -0300

    Adds tests for computed configurations and load config_from.ex while tests are running

--- test/strategy/auth0/oauth_test.exs
+++ test/strategy/auth0/oauth_test.exs
@@ -5,11 +5,40 @@ defmodule Ueberauth.Strategy.Auth0.OAuthTest do
 
   @test_domain "example-app.auth0.com"
 
-  setup do
-    {:ok, %{client: client()}}
+  describe "when default configurations are used" do
+    setup do
+      {:ok, %{client: client()}}
+    end
+
+    test "creates correct client", %{client: client} do
+      asserts_client_creation(client)
+    end
+
+    test "raises when there is no configuration" do
+      assert_raise(RuntimeError, ~r/^Expected to find settings under.*/, fn ->
+        client(otp_app: :unknown_auth0_otp_app)
+      end)
+    end
   end
 
-  test "creates correct client", %{client: client} do
+  describe "when custom/computed configurations are used" do
+    setup do
+      Mix.Config.read!("test/support/config_from.ex")
+      {:ok, %{client: client()}}
+    end
+
+    test "creates correct client", %{client: client} do
+      asserts_client_creation(client)
+    end
+
+    test "raises when there is no configuration" do
+      assert_raise(RuntimeError, ~r/^Expected to find settings under.*/, fn ->
+        client(otp_app: :unknown_auth0_otp_app)
+      end)
+    end
+  end
+
+  defp asserts_client_creation(client) do
     assert client.client_id == "clientidsomethingrandom"
     assert client.client_secret == "clientsecret-somethingsecret"
     assert client.redirect_uri == ""
@@ -17,11 +46,5 @@ defmodule Ueberauth.Strategy.Auth0.OAuthTest do
     assert client.authorize_url == "https://#{@test_domain}/authorize"
     assert client.token_url == "https://#{@test_domain}/oauth/token"
     assert client.site == "https://#{@test_domain}"
-  end
-
-  test "raises when there is no configuration" do
-    assert_raise(RuntimeError, ~r/^Expected to find settings under.*/, fn ->
-      client(otp_app: :unknown_auth0_otp_app)
-    end)
   end
 end
